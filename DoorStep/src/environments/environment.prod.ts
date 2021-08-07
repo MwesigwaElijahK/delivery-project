4f26711bff80b7112474555f02ce442ca690d9bb
MwesigwaElijahK <elijahjonathan22@gmail.com>
2021-08-07 13:44:40 +0300

    delivery-project

---
+++ DoorStep/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/quotes */
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBp5b-qqeWDqQlyS0LTQEA3699r35fJxrs",
+    authDomain: "doorstep-88e14.firebaseapp.com",
+    databaseURL: "https://doorstep-88e14-default-rtdb.firebaseio.com",
+    projectId: "doorstep-88e14",
+    storageBucket: "doorstep-88e14.appspot.com",
+    messagingSenderId: "96068169475",
+    appId: "1:96068169475:web:ae6c22d1ec83b19e948336",
+    measurementId: "G-K6VTPNGW1D"
+  }
+};
---
+++ DoorStep/src/environments/environment.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/quotes */
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+    firebase:{
+      apiKey: "AIzaSyBp5b-qqeWDqQlyS0LTQEA3699r35fJxrs",
+      authDomain: "doorstep-88e14.firebaseapp.com",
+      databaseURL: "https://doorstep-88e14-default-rtdb.firebaseio.com",
+      projectId: "doorstep-88e14",
+      storageBucket: "doorstep-88e14.appspot.com",
+      messagingSenderId: "96068169475",
+      appId: "1:96068169475:web:ae6c22d1ec83b19e948336",
+      measurementId: "G-K6VTPNGW1D"
+    }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
